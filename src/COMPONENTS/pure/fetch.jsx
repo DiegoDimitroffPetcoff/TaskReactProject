2ef4ec3bff1245c5f7721733331f5e4d0180e752
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-17 15:16:33 +0000

    fetch

--- src/COMPONENTS/pure/fetch.jsx
+++ src/COMPONENTS/pure/fetch.jsx
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 
 export const Greetingstyled = (props) => {
 
-
+let url = 'https://jsonplaceholder.typicode.com/users'
+fetch(url)
+.then(response=> response.json())
+.then(data=> console.assert(data))
+.catch(error=> console.log(error))
 
 
     return (
         <div >
 
 ejemplo
+`${data}`
 
         </div>
     );
