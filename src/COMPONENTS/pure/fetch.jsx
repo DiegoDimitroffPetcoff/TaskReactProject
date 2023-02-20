972bc65ef172cbbe66b4f13a30b4fbe45074bd05
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-20 09:25:40 +0000

    Trying template

--- src/COMPONENTS/pure/fetch.jsx
+++ src/COMPONENTS/pure/fetch.jsx
@@ -14,6 +14,13 @@ fetch(url)
         <div >
 
 ejemplo
+<form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
 
 
         </div>
