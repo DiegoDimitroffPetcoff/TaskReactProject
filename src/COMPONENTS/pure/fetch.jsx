3aaa40b7d1fcdd28dc7d578b1ae4997d14523b51
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-20 10:13:00 +0000

    add h1

--- src/COMPONENTS/pure/fetch.jsx
+++ src/COMPONENTS/pure/fetch.jsx
@@ -13,14 +13,8 @@ fetch(url)
     return (
         <div >
 
-ejemplo
-<form action="/action_page.php">
-  <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname"><br><br>
-  <label for="lname">Last name:</label>
-  <input type="text" id="lname" name="lname"><br><br>
-  <input type="submit" value="Submit">
-</form>
+<h1>ejemplo</h2>
+
 
 
         </div>
