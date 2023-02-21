6c1c61b7de3ec02e84ca2dfc6c3cd2065176a679
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-02-21 11:02:28 +0000

    Try fetch with class

--- src/COMPONENTS/pure/fetch.jsx
+++ src/COMPONENTS/pure/fetch.jsx
@@ -1,22 +1,39 @@
-import React  from "react";
-
-
-export const Fetch = (props) => {
-
-let url = 'https://nestjstuits.onrender.com/tuits'
-fetch(url)
-.then(response=> response.json())
-.then( data=> console.assert(data))
-.catch(error=> console.log(error))
-
-
-    return (
-        <div >
-
-<h1>EJEMPLO NUEVO</h1>
+import React, { Component } from "react";
 
 
 
-        </div>
-    );
-}
+
+export class Fetch extends Component {
+    constructor() {
+        super();
+
+
+    }
+
+    componentDidMount() {
+
+        let url = 'https://nestjstuits.onrender.com/tuits'
+        fetch(url)
+            .then(response => {
+                return response.json();
+    })
+        .then(response=> { 
+            console.log(response)
+        })
+
+        .catch (error=> console.log(error))
+
+    }
+
+    render(){
+        return(
+            <div>
+                Nuevo Fetch con class
+            </div>
+        )
+    }
+
+
+  
+ 
+  }
